262af5415c39e56fa594bbe38eafa452891ce698
Jussi Kuosa <jussikuosa@Jussis-MacBook-Pro.local>
2015-10-27 14:13:47 +0200

    Nearby lunch places in Leppävaara

--- cafes.py
+++ cafes.py
@@ -1,77 +1,18 @@
-from constants import NEPALESE, HIMA_SALI, DYLAN_MILK, FACTORY_SALMISAARI, PIHKA, ANTELL, POIJU, THAI, ASIAN, SODEXO_ACQUA, SODEXO_EXPLORER
+from constants import DAYBREAK, SODEXO_TERRA
 
 CAFES = {
-    NEPALESE: {
-        'address': 'Itämerenkatu 12',
-        'menu': '',
-        'distance': 5,
-        'once_per_week': True,
-        'preferred_weekdays': [2, 4], #Wed, Fri,
-        'rating': 3
-    },
-    HIMA_SALI: {
-        'address': 'Tallberginkatu 1 C',
-        'menu': '',
-        'distance': 1,
-        'rating': 2.5
-    },
-    DYLAN_MILK: {
-        'address': 'Porkkalankatu 5',
-        'menu': '',
-        'distance': 4,
-        'rating': 3
-    },
-    FACTORY_SALMISAARI: {
-        'address': 'Salmisaarenaukio 1',
-        'menu': '',
-        'distance': 4,
-        'rating': 3
-    },
-    PIHKA: {
-        'address': 'Itämerenkatu 1',
-        'menu': '',
-        'distance': 5,
-        'rating': 3
-    },
-    ANTELL: {
-        'address': 'Itämerenkatu 15',
-        'menu': '',
-        'distance': 4,
-        'rating': 3
-    },
-    POIJU: {
-        'address': 'Jaalaranta 5',
-        'menu': '',
-        'distance': 3,
-        'once_per_week': True,
-        'rating': 2
-    },
-    THAI: {
-        'address': 'Itämerenkatu 5',
-        'menu': '',
-        'distance': 5,
-        'once_per_week': True,
-        'rating': 3
-    },
-    ASIAN: {
-        'address': 'Itämerenkatu 21',
-        'menu': '',
-        'distance': 3,
-        'once_per_week': True,
-        'rating': 3
-    },
-    SODEXO_ACQUA: {
-        'address': 'Tammasaarenkatu 1-5',
+    DAYBREAK: {
+        'address': 'Linnoitustie 2-4',
         'menu': '',
         'distance': 1,
         'once_per_week': True,
         'rating': 1
     },
-    SODEXO_EXPLORER: {
-        'address': 'Tammasaarenkatu 1-5',
+    SODEXO_TERRA: {
+        'address': 'Lars Sonckin kaari 10-16',
         'menu': '',
         'distance': 1,
         'once_per_week': True,
         'rating': 1
-    }
+    },
 }
--- find_cafe.py
+++ find_cafe.py
@@ -6,7 +6,7 @@
 
 from chief_lunch_officer import ChiefLunchOfficer, WeatherOpinion, FoodTaste
 from constants import TEMPERATURE, PRECIPITATION_CHANCE, PRECIPITATION_AMOUNT, WIND
-from constants import NEPALESE, HIMA_SALI, DYLAN_MILK, FACTORY_SALMISAARI, PIHKA, ANTELL, SODEXO_ACQUA, SODEXO_EXPLORER
+from constants import DAYBREAK, SODEXO_TERRA
 from preferences import FOOD_PREFERENCES
 from cafes import CAFES
 from decorators import get_ignore_errors_decorator
@@ -20,14 +20,13 @@ import re
 
 EmptyMenuOnError = get_ignore_errors_decorator(default_value='No menu. Data feed format for the cafe changed?')
 
-HIMA_SALI_URL = 'http://www.himasali.com/p/lounaslista.html'
-DYLAN_MILK_URL = 'http://dylan.fi/milk/'
-PIHKA_URL = 'http://ruoholahti.pihka.fi/en/'
-FACTORY_SALMISAARI_URL = 'http://www.ravintolafactory.com/ravintolat/helsinki-salmisaari/'
-ANTELL_URL = 'http://www.antell.fi/lounaslistat/lounaslista.html?owner=146'
+
+DAYBREAK_URL = 'http://www.compass-group.fi/Ravintolat/Espoo/Ravintola-Quartetto/Lounaslista/'
+#SODEXO_TERRA_URL = 'http://www.sodexo.fi/ruokalistat/output/daily_json/546/%s/fi'
+SODEXO_TERRA_URL = 'http://www.sodexo.fi/carte/load/html/546/%s/day'
 YLE_WEATHER_FORECAST_URL = 'http://yle.fi/saa/resources/ajax/saa-api/hourly-forecast.action?id=642554'
-SODEXO_ACQUA_URL = 'http://www.sodexo.fi/carte/load/html/30/%s/day'
-SODEXO_EXPLORER_URL = 'http://www.sodexo.fi/carte/load/html/31/%s/day'
+#SODEXO_ACQUA_URL = 'http://www.sodexo.fi/carte/load/html/30/%s/day'
+#SODEXO_EXPLORER_URL = 'http://www.sodexo.fi/carte/load/html/31/%s/day'
 
 def make_readable(content_with_html_tags, insert_new_lines=True, collapse_whitespace=False):
     content_with_html_tags = re.sub('<br.*?>', '\n' if insert_new_lines else '', content_with_html_tags)
@@ -60,44 +59,19 @@ def find_menu(url, date, regex, index=0):
         return found[index]
 
 @EmptyMenuOnError
-def get_sodexo_explorer_menu(date):
-    menu_url = SODEXO_EXPLORER_URL % (date.strftime('%Y-%m-%d'))
+def get_sodexo_terra_menu(date):
+    menu_url = SODEXO_TERRA_URL % (date.strftime('%Y-%m-%d'))
+    print(menu_url)
     menu = find_menu(menu_url, date, '(.*)')
     menu = json.loads(menu)['foods']
     return menu
 
 @EmptyMenuOnError
-def get_sodexo_acqua_menu(date):
-    menu_url = SODEXO_ACQUA_URL % (date.strftime('%Y-%m-%d'))
-    menu = find_menu(menu_url, date, '(.*)')
-    menu = json.loads(menu)['foods']
-    return menu
-
-@EmptyMenuOnError
-def get_antell_menu(date):
+def get_daybreak_menu(date):
     weekday = date.weekday()
-    return find_menu(ANTELL_URL, date, r'<h2[^>]+>(.*?)<img', weekday)
-
-@EmptyMenuOnError
-def get_hima_sali_menu(date):
-    date_label = '%d\\.%d\\.' % (date.day, date.month)
-    return find_menu(HIMA_SALI_URL, date, r'%s(.*?Wok.*?[\d\.]+)' % (date_label), -1)
-
-@EmptyMenuOnError
-def get_dylan_milk_menu(date):
-    return find_menu(DYLAN_MILK_URL, date, r'<div class="fbf_desc">(.*?)</div>')
-
-@EmptyMenuOnError
-def get_pihka_menu(date):
-    weekday = date.weekday()
-    found = get_and_find_all(PIHKA_URL, r'<div class="menu\-day.*?<ul>(.*?)</div>')
-    return found[weekday]
-
-@EmptyMenuOnError
-def get_factory_salmisaari_menu(date):
-    date_label = date.strftime('%d.%m.%Y')
-    found = get_and_find_all(FACTORY_SALMISAARI_URL, r'%s</h3>(.*?)</p>' % (date_label))
-    return found[0]
+    start = date.today().strftime("%A")
+    end = (date.today() + timedelta(1)).strftime("%A")
+    return find_menu(DAYBREAK_URL, date, r'%s(.*?)<strong>.*%s' % (start, end))
 
 def get_todays_weather():
     weather_response = get(YLE_WEATHER_FORECAST_URL)
@@ -148,23 +122,12 @@ def update_history(history, today, todays_cafe):
     store_history(history)
 
 today = date.today()
-#today = today + timedelta(days=2)
 print('Today %s\n' % today.strftime('%d.%m.%Y'))
 
-sodexo_acqua_menu = get_sodexo_acqua_menu(today)
-print('\nSodexo Acqua:\n\n%s' % make_readable(sodexo_acqua_menu, collapse_whitespace=True))
-sodexo_explorer_menu = get_sodexo_explorer_menu(today)
-print('\nSodexo Explorer:\n\n%s' % make_readable(sodexo_explorer_menu, collapse_whitespace=True))
-antell_menu = get_antell_menu(today)
-print('\nAntell:\n\n%s' % make_readable(antell_menu, collapse_whitespace=True))
-hima_sali_menu = get_hima_sali_menu(today)
-print('\nHima & Sali:\n\n%s' % make_readable(hima_sali_menu, insert_new_lines=False))
-dylan_milk_menu = get_dylan_milk_menu(today)
-print('\nDylan Milk:\n\n%s' % make_readable(dylan_milk_menu))
-pihka_menu = get_pihka_menu(today)
-print('\nPihka:\n\n%s' % make_readable(pihka_menu, collapse_whitespace=True))
-factory_salmisaari_menu = get_factory_salmisaari_menu(today)
-print('\nFactory Salmisaari:\n\n%s' % make_readable(factory_salmisaari_menu, insert_new_lines=False))
+sodexo_terra_menu = get_sodexo_terra_menu(today)
+print('\nSodexo Terra:\n\n%s' % make_readable(sodexo_terra_menu, collapse_whitespace=True))
+daybreak_menu = get_daybreak_menu(today)
+print('\nDayBreak:\n\n%s' % make_readable(daybreak_menu, collapse_whitespace=True))
 
 weather = get_todays_weather()
 print('\nWeather:\n\n temperature %s C\n chance of precipitation %s percent\n precipitation amount %s mm\n wind %s m/s' % (weather[TEMPERATURE], weather[PRECIPITATION_CHANCE], weather[PRECIPITATION_AMOUNT], weather[WIND]))
@@ -174,13 +137,8 @@ current_week_cafes = ordered_cafes(lunch_history)
 print('\nLunch history for current week:\n\n %s' % ', '.join(current_week_cafes))
 
 cafes = deepcopy(CAFES)
-cafes[SODEXO_EXPLORER]['menu'] = sodexo_explorer_menu
-cafes[SODEXO_ACQUA]['menu'] = sodexo_acqua_menu
-cafes[ANTELL]['menu'] = antell_menu
-cafes[HIMA_SALI]['menu'] = hima_sali_menu
-cafes[DYLAN_MILK]['menu'] = dylan_milk_menu
-cafes[PIHKA]['menu'] = pihka_menu
-cafes[FACTORY_SALMISAARI]['menu'] = factory_salmisaari_menu
+cafes[SODEXO_TERRA]['menu'] = sodexo_terra_menu
+cafes[DAYBREAK]['menu'] = daybreak_menu
 
 food_taste = FoodTaste().preferences(FOOD_PREFERENCES)
 weather_opinion = WeatherOpinion().weather(weather)
